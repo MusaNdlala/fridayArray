45969af1dfa6833b0c6ef4d125de6092f4550f5d
Musa <Musa@DESKTOP-N08JB8A>
2021-09-10 15:02:50 +0200

    Add project files.

---
+++ fridayArray/Program(1).cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace fridayArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            nameMark();
+        }
+        static void nameMark()
+        {
+            string[] name = new string[5];
+            int[] Mark = new int[5];
+            int i = 0;
+            while (i < name.Length)
+            {
+                Console.WriteLine("Please enter name ");
+                name[i++] = Console.ReadLine();
+            }
+            i = 0;
+            while (i < Mark.Length)
+            {
+                Console.WriteLine("Please enter Mark for {0}", name[i]);
+                Mark[i++] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("name     Mark");
+            i = 0;
+            foreach (int n in Mark)
+            {
+                Console.WriteLine("" + name[i].ToString() + "     " + Mark[i++].ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
